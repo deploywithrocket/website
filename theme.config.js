92502c8573c76178a8778418af90bfeb9ca8cbf3
Simon Rubuano <sr@mgk.dev>
2020-12-11 04:51:49 +0100

    🚧 Update colors

--- theme.config.js
+++ theme.config.js
@@ -11,11 +11,11 @@ const main = new Theme()
             default: '#FD3A84',
             muted: '#BA4572',
         },
-        button: '#FEE5EF',
+        button: '#EEEEEE',
         background: '#FFFFFF',
         navigation: {
-            default: '#F7EDF1',
-            hover: new TinyColor('#F7EDF1').setAlpha(0.75),
+            default: '#F0F0F0',
+            hover: new TinyColor('#F0F0F0').setAlpha(0.75),
         },
         sidebar: {
             active: '#FFE5EF',
@@ -23,25 +23,25 @@ const main = new Theme()
         'on-background': {
             default: '#2F0515',
             code: '#FFF1F6',
-            blockquote: '#9A828B',
-            figcaption: '#9A828B',
-            list: '#9A828B',
+            blockquote: '#8E8E8E',
+            figcaption: '#8E8E8E',
+            list: '#8E8E8E',
             muted: '#F0E2E7',
         },
         'on-navigation': {
-            default: '#2F0515',
-            icon: new TinyColor('#2F0515').setAlpha(0.75),
-            muted: new TinyColor('#2F0515').setAlpha(0.1),
+            default: '#4F4F4F',
+            icon: new TinyColor('#4F4F4F').setAlpha(0.75),
+            muted: new TinyColor('#4F4F4F').setAlpha(0.1),
         },
         'on-sidebar': {
             default: '#6F4E5B',
             active: '#B94571',
-            header: '#C0A0AC',
+            header: '#ABABAB',
             hover: '#B94571',
         },
         'on-brand': '#FFFFFF',
         'on-button': {
-            default: '#B07A8F',
+            default: '#8D8D8D',
             hover: '#B94571',
         },
     })
@@ -58,11 +58,11 @@ const dark = new Theme()
             default: '#FD3A84',
             muted: '#BA4572',
         },
-        button: '#32252A',
-        background: '#261C20',
+        button: '#292929',
+        background: '#1F1F1F',
         navigation: {
-            default: '#472D37',
-            hover: new TinyColor('#472D37').setAlpha(0.75),
+            default: '#363636',
+            hover: new TinyColor('#363636').setAlpha(0.75),
         },
         sidebar: {
             active: '#2D2126',
@@ -70,9 +70,9 @@ const dark = new Theme()
         'on-background': {
             default: '#EFEFEF',
             code: '#2F2328',
-            blockquote: '#84626F',
-            figcaption: '#84626F',
-            list: '#84626F',
+            blockquote: '#8C8C8C',
+            figcaption: '#8C8C8C',
+            list: '#8C8C8C',
             muted: '#32252A',
         },
         'on-navigation': {
@@ -88,7 +88,7 @@ const dark = new Theme()
         },
         'on-brand': '#860C3B',
         'on-button': {
-            default: '#AF8293',
+            default: '#919191',
             hover: '#FFCBDF',
         },
     })
